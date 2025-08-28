04865d598cb5be9b4defaecc779aaed464c1753a
AlexM <wmatallana28@gmail.com>
2025-08-28 17:10:41 -0500

    Cambios

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hola Clase");
+
+        System.out.println("COMANDOS GIT");
+        System.out.println("git status - estado rama");
+        System.out.println("git branch - ver las ramas");
+        System.out.println("git add - agrupar archivos");
+        System.out.println("git push origin - subir cambios ");
+        System.out.println("git commit -m - guardar cambios local");
+        System.out.println("git config -- list - listar configuracion");
+        System.out.println("git log - ver todos los commit");
+        System.out.println("git clone - clonar proyecto");
+        System.out.println("git fetch - ver ramas remotas");
+        System.out.println("git switch - para crear y posicionarme sobre la rama");
+
+
+
+
+
+
+
+    }
+}
